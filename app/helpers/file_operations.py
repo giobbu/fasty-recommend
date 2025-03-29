06055caf4b10e275dc5c23276c4a04d71cb03b4f
giobbu <giovanniburoni@gmail.com>
2025-03-29 18:53:59 +0100

    add router for adding new description

--- app/helpers/file_operations.py
+++ app/helpers/file_operations.py
@@ -1,4 +1,6 @@
 import json
+import os
+from loguru import logger
 from fastapi import HTTPException
 
 # Helper function to load a JSON file
@@ -16,10 +18,21 @@ def save_json(file_path: str, data: dict):
             json.dump(data, file)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to save data: {str(e)}")
-    
+
 # Helper function to validate inputs
 def validate_input(value, expected_type, field_name):
     if not value:
         raise HTTPException(status_code=400, detail=f"No {field_name} provided")
     if not isinstance(value, expected_type):
         raise HTTPException(status_code=400, detail=f"{field_name} must be a {expected_type.__name__}")
+
+def delete_tfidf_files(path_1: str, path_2: str):
+    """
+    Delete the TF-IDF files if they exist.
+    """
+    if os.path.isfile(path_1) or os.path.isfile(path_2):
+        os.remove(path_1)
+        logger.debug(f"Deleted {path_1}")
+        os.remove(path_2)
+        logger.debug(f"Deleted {path_2}")
+
--- app/routes/descriptions.py
+++ app/routes/descriptions.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
-from app.helpers.file_operations import load_json, save_json, validate_input
+from app.helpers.file_operations import load_json, save_json, validate_input, delete_tfidf_files
 from app.config import Config
+
 
 router = APIRouter()
 
@@ -19,12 +20,13 @@ def add_description(description: dict):
     Add a new description.
     """
     data = description.get("data")
-    validate_input(data, dict, "data")
+    validate_input(data, str, "data")
     documents = load_json(Config.doc_db_path)
-
     if data in documents.values():
-        raise HTTPException(status_code=400, detail="Document already exists")
-
-    documents[str(len(documents) + 1)] = data
+        raise HTTPException(status_code=400, 
+                            detail="Document already exists")
+    documents[f"doc{str(len(documents) + 1)}"] = data
     save_json(Config.doc_db_path, documents)
+    delete_tfidf_files(Config.doc_word_path, 
+                    Config.vectorizer_path)
     return JSONResponse(status_code=201, content={"message": "Description added successfully", "data": data})
